df1c530589d5d1495be6550ea5933ca6934d89eb
dekasthiti <dekasthiti@gmail.com>
2021-07-10 17:21:58 -0700

    [RealPython] Cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,49 @@
+"""
+Caesar Cipher (caesar.py)
+
+A Caesar cipher is a simple substitution cipher in which each letter of the plain text is substituted with a letter found by moving n places down the alphabet. For example, assume the input plain text is the following:
+
+abcd xyz
+If the shift value, n, is 4, then the encrypted text would be the following:
+
+efgh bcd
+You are to write a function that accepts two arguments, a plain-text message and a number of letters to shift in the cipher. The function will return an encrypted string with all letters transformed and all punctuation and whitespace remaining unchanged.
+
+Note: You can assume the plain text is all lowercase ASCII except for whitespace and punctuation.
+"""
+
+
+def caesar_cipher(plain_text='', shift_value=0):
+    new_plain_text = ''
+    for letter in plain_text:
+        if not letter.isalpha():
+            new_plain_text += letter
+        else:
+            # What happens when we cross 'z' : either the shift_value is big or because we have the
+            # last letters of the alphabet?
+            new_plain_text += chr(ord(letter) + shift_value)
+    return new_plain_text
+
+
+def caesar_cipher_with_stdlib(plain_text='', shift_value=0):
+    import string
+    # 1. First, create a translation table with the shift
+    # Create the translation map
+    letters = string.ascii_lowercase
+    shifted_letters = letters[shift_value:] + letters[:shift_value]
+    translation_table = str.maketrans(letters, shifted_letters)
+    return plain_text.translate(translation_table)
+
+
+def main():
+    plain_text = 'hello'
+    
+    assert caesar_cipher(plain_text, 4) == 'lipps'
+    
+    print(f'Cipher for {plain_text} is {caesar_cipher(plain_text, 4)}')
+    print(f'Cipher for {plain_text} is {caesar_cipher_with_stdlib(plain_text, 4)}')
+    
+    
+if __name__ == '__main__':
+    main()
+
